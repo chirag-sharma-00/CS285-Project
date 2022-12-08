17f8b4abea4096e636a8c4971717adcdb9cd5af0
Harvey Hu <yubinhu@126.com>
2022-12-07 18:04:10 -0800

    first sac run finished

--- peersac.sh
+++ peersac.sh
@@ -3,5 +3,5 @@ python cs285/scripts/run_sac_experiment.py \
 --discount 0.99 --scalar_log_freq 1500 \
 -n 2000000 -l 2 -s 256 -b 1500 -eb 1500 \
 -lr 0.0003 --init_temperature 0.1 --exp_name sac_hw3q6_hprm_3agents \
---seed 41 --actor_update_frequency 10 --critic_epsilon 0.3 \
---num_agents 3 --advice_dim 3
+--seed 42 --actor_update_frequency 10 --critic_epsilon 0.3 \
+--num_agents 3 --advice_dim 3 # --save_params
