68c3e38d1c4ca78ae6155e5ee5da5fa404025a53
Harvey Hu <yubinhu@126.com>
2022-12-09 09:25:29 -0800

    peer sac. results in plotter. Needs to train more

--- peersac.sh
+++ peersac.sh
@@ -2,7 +2,7 @@ python cs285/scripts/run_sac_peer_experiment.py \
 --env_name HalfCheetah-v4 --ep_len 150 \
 --discount 0.99 --scalar_log_freq 1 \
 --n_iter 200 --n_layers 2 --size 256 --batch_size 1500 --eval_batch_size 1500 \
--lr 0.0003 --init_temperature 0.1 --exp_name sac_peer_2_agents \
+-lr 0.0003 --init_temperature 0.1 --exp_name sac_peer2_agents_rpb20000_temp0.1 \
 --critic_epsilon 0.3 --advice_net_n_layers 1 --advice_net_size 16 --advice_dim 4
 --seed 42 --actor_update_frequency 10 \
 --num_agents 2 # --save_params
