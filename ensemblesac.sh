6c73c477fce0f29ecd9d39966b529f8f82f3e478
Chirag Sharma <shchirag@berkeley.edu>
2022-12-08 23:28:55 -0800

    ensemble and peer learning test comparison

--- ensemblesac.sh
+++ ensemblesac.sh
@@ -1,7 +1,7 @@
 python cs285/scripts/run_sac_ensemble_experiment.py \
 --env_name HalfCheetah-v4 --ep_len 150 \
---discount 0.99 --scalar_log_freq 1500 \
---n_iter 2000000 --n_layers 2 --size 256 --batch_size 1500 --eval_batch_size 1500 \
+--discount 0.99 --scalar_log_freq 1 \
+--n_iter 200 --n_layers 2 --size 256 --batch_size 1500 --eval_batch_size 1500 \
 -lr 0.0003 --init_temperature 0.1 --exp_name sac_ensemble_2_agents \
 --seed 42 --actor_update_frequency 10 \
 --num_agents 2 # --save_params
