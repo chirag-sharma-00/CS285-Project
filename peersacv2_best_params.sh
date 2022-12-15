70517fb7a3c9c0c93ed3e8d05bd0f8377a531725
Harvey Hu <yubinhu@126.com>
2022-12-14 21:28:12 -0800

    all graphs finished

--- peersacv2_best_params.sh
+++ peersacv2_best_params.sh
@@ -6,5 +6,5 @@ python cs285/scripts/run_sac_peer_experiment.py \
 -n 800000 -l 2 -s 256 -b 1500 -eb 1500 \
 -lr 0.0003 --init_temperature 0.2 --exp_name peersac_bestparams \
 --seed $seed --actor_update_frequency 10 \
---critic_epsilon 0.7 --advice_dim 4 --num_agents 3 --critic_version 2 
+--critic_epsilon 0.7 --advice_dim 2 --num_agents 2 --critic_version 2 
 
